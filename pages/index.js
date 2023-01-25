fa746b56f67a854bb3d9bf65701c9fcbb95e76b8
Nikolay Shabaev <nikolai.shabaev@mail.ru>
2023-01-25 19:28:07 +0700

    feat: Создал адаптив popup. Добавил файлы

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+const popup = document.querySelector ('.popup');
+const buttonEditOpen = document.querySelector ('.profile__button_edit');
+const buttonClose = document.querySelector ('.popup__button_action_close');
+const getName = document.querySelector ('.profile__name');
+const getDescription = document.querySelector ('.profile__text');
+const editForm = document.querySelector ('.popup__container');
+let nameInput = editForm.querySelector ('.popup__input_name');
+let descriptionInput =  editForm.querySelector ('.popup__input_description');
+
+function openPopup () {
+  popup.classList.add ('popup__open');
+  nameInput.value = getName.textContent;
+  jobInput.value = getDescription.textContent;
+}
+
+function closePopup ()  {
+popup.classList.remove ('popup__open');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    getName.textContent = `${nameInput.value}`;
+    getDescription.textContent = `${descriptionInput.value}`;
+    closePopup ();
+}
+
+buttonEditOpen.addEventListener ('click', openPopup);
+buttonClose.addEventListener ('click', closePopup);
+editForm.addEventListener('submit', formSubmitHandler);
